bf411f767b8a4ff2e01e98dc6bb9ed374528cad4
翁献山 <1072724451@qq.com>
2017-03-13 20:43:11 +0800

    添加设置界面

--- src/router/router.js
+++ src/router/router.js
@@ -5,11 +5,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 const App = resolve => require(['../App'], resolve)
+const Setupthe = resolve => require(['../pages/Setupthe'], resolve)
 const proRootDic = '/studentmanagemet';
-const routes = [{
-    path: proRootDic, component: App, meta: {requiresAuth: true}
-},
-    {path: '*', redirect: proRootDic}];
+const Home= resolve => require(['../pages/Home'],resolve)
+
+
+/*配置路由目录*/
+const routes = [
+    {path: proRootDic, component: App, meta: {requiresAuth: true}},
+    {path: proRootDic + '/setupthe', component: Setupthe, meta: {requiresAuth: true}},
+    {path: proRootDic + '/home',component:Home,meta :{requiresAuth:true}},
+    {path: '*', redirect: proRootDic}/*默认路由*/
+];
+
+
 const router = new VueRouter({
     mode: 'history',
     routes,
