9abbea143e0ee1beb8b5a844118af37e1a0d7743
Baird-weng <644672334@qq.com>
2017-04-02 21:36:16 +0800

    修复地区格式。

--- src/pages/settingpage/Bindingschool.vue
+++ src/pages/settingpage/Bindingschool.vue
@@ -1,20 +1,16 @@
 <template>
     <div>
         <group>
-            <x-input v-model="info.contact" style="height: 40px" placeholder="请输入联系人姓名" title="联系人姓名"></x-input>
-            <x-input v-model="info.tel" style="height: 40px" placeholder="请输入联系人电话" title="联系人电话"></x-input>
-            <x-input v-model="info.name" style="height: 40px" placeholder="请输入学校全称" title="所属 学校"></x-input>
-            <x-address :title="title2" placeholder="请选择学校地址" v-model="info.address" raw-value :list="addressData"
-                       value-text-align="left"
-            >
-            </x-address>
-            <x-textarea v-model="info.detailedaddress" placeholder="请填写详细地址"></x-textarea>
+            <x-input v-model="infoData.contact" style="height: 40px" placeholder="请输入联系人姓名" title="联系人姓名"></x-input>
+            <x-input v-model="infoData.tel" style="height: 40px" placeholder="请输入联系人电话" title="联系人电话"></x-input>
+            <x-input v-model="infoData.name" style="height: 40px" placeholder="请输入学校全称" title="所属 学校"></x-input>
+            <x-address :title="title2" v-model="addressValue" placeholder="请选择学校地址" raw-value :list="addressData"
+                       value-text-align="left"></x-address>
+            <x-textarea v-model="infoData.detailedaddress" placeholder="请填写详细地址"></x-textarea>
         </group>
-
         <div style="margin-left: 10px;margin-right:10px;margin-top: 20px;">
             <x-button class="btn" @click.native="submit" type="primary">提交审核</x-button>
         </div>
-
     </div>
 </template>
 <style>
@@ -37,19 +33,11 @@
     export default{
         data(){
             return {
-                list1: [['广东中山大学', '广东暨南大学', '广东理工大学', '广州风萧萧培训机构',]],
+                list1: [['广东中山大学', '广东暨南大学', '广东理工大学', '广州风萧萧培训机构']],
                 title: "所属学校",
                 title2: "学校地址",
                 addressData: ChinaAddressData,
-                value2: ['广东省', '广州市', '天河区'],
-
-                info: {
-                    contact: '',
-                    tel: '',
-                    name: '',
-                    address: [],
-                    detailedaddress: '',
-                }
+                infoData: {},
             }
         },
         components: {
@@ -59,6 +47,21 @@
             XButton,
             Divider,
             Cell, XInput, XAddress, XTextarea
+        },
+        //计算属性。
+        computed: {
+            //获取地区的值。类似这样的格式 440000|440100|440118
+            addressValue(){
+                if(this.infoData.address){
+                    let code = this.infoData.address.split('|');
+                    if(code.length>0){
+                        return code;
+                    }
+                }
+                else {
+                    return ['广东省','广州市','天河区'];
+                }
+            }
         },
         methods: {
             reloadData: function () {
@@ -71,43 +74,25 @@
                 });
                 this.getschoolinfo();
             },
-            getschoolinfo:function () {
-                let that = this;
-
+            getschoolinfo() {
+                var that = this;
                 Service.getschoolinfo({}).then(function (response) {
                     if (response.data && response.data.data) {
-
-                        that.info = response.data.data;
-                        that.info.contact = response.data.data.contact;
-                        that.info.tel = response.data.data.tel;
-                        that.info.name = response.data.data.name;
-                        that.info.detailedaddress = response.data.data.detailedaddress;
-                        that.info.contact = response.data.data.contact;
-                        var array=response.data.data.address;
-                        var address=array.split(" ");
-                        that.info.address=address;
-                        console.log(that.info.address);
-
-
+                        let info = response.data.data;
+                        that.infoData = info;
                     }
                 })
             },
             submit(){
-
-
+                console.log(this.addressValue);
                 let that = this;
                 Service.gettecherinfobyid({teacherid: '1'}).then(function (response) {
-
                     if (response.data && response.data.result == '1') {
                         that.getschoolinfo();
                     }
-
                 });
 
-
 //                this.$router.push({name: 'msg'});
-
-
             }
         }
     }
